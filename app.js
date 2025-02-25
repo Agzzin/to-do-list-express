013c7f97e21d5f6e2d9d5611c4c0f944394020c0
Adryan Gonçalves <agzzin06@gmail.com>
2025-02-25 14:37:16 -0300

    'Instalação-nodemon'

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 const express = require('express')
+
+const App = express();
+
+App.get('/', (req, res =>{
+    res.send('<h1>Minha lista de tarefas</h1>')
+}))
+
+App.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000')
+})
