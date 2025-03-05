43934b4ed17ee0d202c69cf3aa3db7081ea90c0a
Adryan Gonçalves <agzzin06@gmail.com>
2025-03-05 18:26:05 -0300

    Create-logic-data

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router();
 const Checklist = require('../models/checklist');
+const checklist = require('../models/checklist');
 
 router.get('/', async (req, res) => {
     try {
         let checklists = await Checklist.find({});
-        res.status(200).json(checklists)
+        res.status(200).render('checklists/index', {checklists: checklists})
     }catch(error){
         res.status(500).json(req.body)
     }
@@ -17,8 +18,9 @@ router.post('/', async (req, res) => {
    try{
     let checklists = await Checklist.create({name})
     res.status(200).json(checklists);
+    res.status(200).render('checklists/index', {checklists: checklists})
    }catch(error){
-    res.status(422).json(error)
+    res.status(422).render('pages/error/index', {error: 'err0 ao exibir as listas'})
    }
 
 
@@ -28,9 +30,9 @@ router.post('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
     try{
         let checklists = await Checklist.findById(req.params.id);
-        res.status(200).json(checklists)
+        res.status(200).render('checklists/show', {checklist: checklist})
     }catch(error){
-        res.status(500).json(req.body)
+        res.status(500).render('pages/error', {error: 'Erro ao exibir as listas de tarefas'})
     }
 })
 
